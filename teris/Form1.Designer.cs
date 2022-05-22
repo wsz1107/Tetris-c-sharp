a83eefba504771c73f1ff2e059d12893ec1e6fa1
Shitetsu Go <Shitetsu Go@LAPTOP-OJCC1L54>
2022-05-22 14:53:13 +0900

    プロジェクト ファイルを追加します。

---
+++ teris/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace teris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ teris/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace teris
+{
+    public partial class Form1 : Form
+    {
+        public List<Point> puzzlesPos=new List<Point>();
+        public RectPuzzle rp;
+        public int FallSpeed=25;
+
+        public Form1()
+        {
+            InitializeComponent();
+            puzzlesPos.Add(new Point(100, 100));
+            for(int i = 0; i < puzzlesPos.Count; i++)
+            {
+                rp = new RectPuzzle(puzzlesPos[i]);
+            }
+            timer1.Enabled = true;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            e.Graphics.FillPolygon(blueBrush, rp.points);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Debug.WriteLine(1);
+            for (int i=0; i < puzzlesPos.Count; i++)
+            {
+                var entry = puzzlesPos[i];
+                entry.Y+=FallSpeed;
+                puzzlesPos[i] = entry;
+                Debug.WriteLine(puzzlesPos[i].Y);
+            }
+            
+            pictureBox1.Invalidate();
+        }
+    }
+    public class Puzzles
+    {
+        SolidBrush brush;
+    }
+    public class RectPuzzle:Puzzles
+    {
+        public Point[] points;
+        int rectLength = 50;
+        public RectPuzzle(Point _point)
+        {
+            this.points = new Point[]
+            {
+                new Point(_point.X, _point.Y),
+                new Point(_point.X+rectLength, _point.Y),
+                new Point(_point.X+rectLength,_point.Y+rectLength),
+                new Point (_point.X,_point.Y+rectLength)
+            };
+        }
+    }
+
+}
