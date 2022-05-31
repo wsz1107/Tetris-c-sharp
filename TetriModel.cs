d29937c4866601f34331b2c06b9d07fa3b86ec62
SIZHE WU <20ad601@ms.dendai.ac.jp>
2022-05-31 14:36:49 +0900

    add TetrisModel class, Tetrimino class

---
+++ TetriModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class TetrisModel
+{
+	public int[,] CurrentPos;
+	public int FallSpeed;
+	public const int[,] StartPos;
+	public const int[,] StandbyPos;
+
+}
+ public abstract class Tetrimino
+{
+	int pivotX,pivotY;
+	int mode;
+
+	public List<int[,]> points;
+	public abstract List<int[,]> GetPoints(int[,] point, int mode);
+	public void UpdatePoints(List<int[,]> points)
+    {
+		this.points = points;
+    }
+	public void UpdateMode(int newMode)
+    {
+		this.mode= newMode;
+    }
+}
+public class OShapeTetrimino:Tetrimino
+{
+
+}
+public class TShapeTetrimino : Tetrimino
+{
+
+}
+public class ZShapeTetrimino : Tetrimino
+{
+
+}
+public class SShapeTetrimino : Tetrimino
+{
+
+}
+public class LShapeTetrimino : Tetrimino
+{
+
+}
+public class JShapeTetrimino : Tetrimino
+{
+
+}
+public class IShapeTetrimino : Tetrimino
+{
+
+}
